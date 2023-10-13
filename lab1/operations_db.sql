b549fc70ace5001da31b0b53bd3f12d3a4761894
baki <istaeverd3@gmail.com>
2023-10-13 14:33:00 +0300

    lab1: add scheme for db

---
+++ lab1/operations_db.sql
@@ -0,0 +1,22 @@
+CREATE TABLE balance (
+    id serial primary key,
+    create_date timestamp(3),
+    debit numeric(18, 2),
+    credit numeric(18, 2),
+    amount numeric(18, 2)
+);
+
+CREATE TABLE articles (
+    id serial primary key,
+    name varchar(50)
+);
+
+CREATE TABLE operations (
+    id serial primary key,
+    article_id integer references articles(id),
+    debit numeric(18, 2),
+    credit numeric(18, 2),
+    create_date timestamp(3),
+    balance_id integer references balance(id)
+);
+
